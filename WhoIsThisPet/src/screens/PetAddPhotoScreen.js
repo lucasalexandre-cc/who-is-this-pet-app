ef598e3e12940de1a73877e757c93380a5ed19aa
Lucas Alexandre <lucasalexandre.s.cc@gmail.com>
2020-02-02 21:25:27 -0300

    Create upload button to upload a pet image

--- WhoIsThisPet/src/screens/PetAddPhotoScreen.js
+++ WhoIsThisPet/src/screens/PetAddPhotoScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 
 import Header from '../components/Header';
 import CardContainer from '../components/CardContainer';
@@ -7,14 +9,27 @@ import CardContainer from '../components/CardContainer';
 const PetAddPhotoScreen = () => {
   return (
     <View style={{ flex: 1 }}>
-      <Header title="Who is this pet" />
+      <Header title="Adicionar fotos" />
       <CardContainer>
-        <Text>Teste</Text>
+        <TouchableOpacity style={styles.buttonUpload}>
+          <FontAwesomeIcon icon={ faCamera } size={36} style={{ color: 'rgb(229, 84, 41)' }} />
+          <Text style={{ color: 'rgb(229, 84, 41)' }}>ADD FOTO</Text>
+        </TouchableOpacity>
       </CardContainer>
     </View>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonUpload: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 180,
+    height: 100,
+    borderWidth: 2,
+    borderColor: 'rgba(229, 84, 41, .4)',
+    backgroundColor: 'rgba(229, 84, 41, .2)',
+  }
+});
 
 export default PetAddPhotoScreen;
